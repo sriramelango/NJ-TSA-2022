32eb0f243d4bf7ad5f39cd6d6c0cec387b8d3b23
Sriram Elango <sriram@fireflies.ai>
2022-03-29 21:16:55 -0400

    Update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime
 from vega_datasets import data
+from PIL import Image
 
 from utils import db
 
@@ -14,14 +15,40 @@ def space(num_lines=1):
         st.write("")
 
 
-st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting app")
-
-# Data visualisation part
-
-#st.title("💬 Commenting app")
-
+st.set_page_config(layout="wide", page_icon="", page_title="Top 10 Activities/Places in NJ")
+st.title("Best Places and Activites in New Jersey")
+st.markdown("""
+ - New Jersey is filled with amazing places, but here are the top 10 activities and places within the Garden State! 
+ - Feel free to discuss (or debate) in the comment below and explore!
+ - Created as a part of the Coding NJ TSA 2022 Challenge
+ - Made by @sriramelango: https://github.com/sriramelango 
+ """)
 conn = db.connect()
 comments = db.collect(conn)
+
+
+
+
+st.subheader("Six Flags Great Adventure/Hurricane Harbor")
+image = Image.open('images/sixflags.jpeg')
+st.image(image, caption = "Six Flags Great Adventure Rollercoaster")
+st.info("""Six Flags Great Adventure and Hurricane Harbor is a park in Jackson, New Jersey! It includes a Wild Safari to top it off! It is filled with rollercoasters, restaurants, and is all around a great family experience!
+More information can be found here: https://www.sixflags.com/greatadventure
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 with st.expander("💬 Open comments"):
 
